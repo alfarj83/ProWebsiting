d0fc239d775f04913009e0749fe24604cdc349b3
alfarj83 <148643005+alfarj83@users.noreply.github.com>
2024-07-26 20:17:12 -0400

    Added a popup

---
+++ main.js
@@ -0,0 +1,8 @@
+//adds functionality to popup close button
+document.addEventListener('DOMContentLoaded', (event) => {
+    const popup = document.getElementById('popup');
+});
+
+function closePopup() {
+    popup.style.display = 'none';
+}
